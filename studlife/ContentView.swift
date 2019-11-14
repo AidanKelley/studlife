e78a0ace46df3be49d49efe33e8f735c4a267d06
Aidan Kelley <aidanmkelley@gmail.com>
2019-11-14 02:44:54 -0600

    Initial Commit

---
+++ studlife/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  studlife
+//
+//  Created by Aidan Kelley on 11/14/19.
+//  Copyright © 2019 Aidan Kelley. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
